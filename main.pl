6239c0a376c59501a6ea26cb188b8984715a39ae
Lujine <lujineelfeky@gmail.com>
2020-12-19 14:56:25 +0200

    feat: add base code for creating rooms/apartments

---
+++ main.pl
@@ -0,0 +1,129 @@
+:-use_module(library(clpfd)).
+:-include('rectangle_helpers.pl').
+
+/**********************************************************************
+*******************************Structs*********************************
+Generic rectangle:
+	r(UpperLeftCornerX, Width, UpperLeftCornerY, Height)
+Apartment Type:
+	apt_type(Num, RoomsTypes, MinRoomSize, Widths, Heights)
+		Num = number of rooms in the apartment
+		RoomsTypes is a list of the room types ex. [bedroom, bathroom, kitchen]
+		MinRoomSize is a list of the minimum size for each room
+		Widths is an optional list of the width of each room
+		Heights is an optional list of the height of each room
+		
+Specific retangles:
+	apt(UpperLeftCornerX, Width, UpperLeftCornerY, Height)
+	hall(UpperLeftCornerX, Width, UpperLeftCornerY, Height)
+**********************************************************************
+**********************************************************************/
+	
+input(FloorWidth, FloorHeight, Landscapes, Open, AptTypes, NumApts, Apartments, Hallways):-
+	% Test Cases
+	/*
+	FloorWidth = 10, 
+	FloorHeight = 10,
+	AptTypes = [
+				apt_type(_,_,[24], _,_),
+				apt_type(_,_,[24], _,_)
+	],
+	NumApts = [2, 2],
+	*/
+	% Domains
+	FloorWidth in 10..500,
+	FloorHeight in 10..500,
+	NumApts ins 1..10,
+
+	% Variables
+	TotalArea #= FloorWidth * FloorHeight,
+	sum(NumApts, #=, TotalNumApts),
+	
+	% Apartment and External Hallways Layout Constraints
+	maplist(createApts(FloorWidth, FloorHeight), AptTypes, NumApts, ApartmentsList, AptCoordList),
+	append(ApartmentsList, Apartments),
+	append(AptCoordList, Coords),
+	
+	createHallways(FloorWidth, FloorHeight, TotalNumApts, NumHallways, Hallways, HallCoordList),
+	
+	append(Apartments, Rooms),
+	append(Rooms, Hallways, Floor),
+	disjoint2(Floor),
+
+	% Utility of Apartments and Hallways
+	apts_util(Apartments, ApartmentsArea),
+	calc_util(Hallways, HallsArea),
+	TotalArea #= ApartmentsArea + HallsArea,
+	
+	append([NumHallways| HallCoordList], Coords, Label),
+	labeling([], Label).
+	
+/****************************createApts*********************************
+creates all apartments of some type "AptType"
+this apartment type exists "NumApts" times on the floor
+***********************************************************************/
+createApts(_, _, _, 0, [], []).	
+createApts(FloorWidth, FloorHeight, AptType, NumApts, [AptH | AptT], Coords):-
+	NumApts #> 0,
+
+	createAptRooms(FloorWidth, FloorHeight, AptType, AptH, CoorH),
+	append(CoorH, CoorT, Coords),
+
+	Counter #= NumApts - 1,
+	createApts(FloorWidth, FloorHeight, AptType, Counter, AptT, CoorT).
+
+/**************************createAptRooms*******************************
+creates the rooms in an apartment
+***********************************************************************/
+createAptRooms(_, _, apt_type(0, _, _, _, _), [], []).
+createAptRooms(FloorWidth, FloorHeight, AptType, [RoomH | RoomT], Coords):-
+
+	%AptType = apt_type(NumRooms, RoomTypes, MinRoomSize, Widths, Heights),
+	AptType = apt_type(NumRooms, [TypeH | TypeT], [MinSizeH | MinSizeT], _, _),
+	NumRooms #> 0,
+	
+	createRoom(FloorWidth, FloorHeight, MinSizeH, RoomH, CoorH),
+	NumRoomsRem #= NumRooms - 1 ,
+	AptTypeRem = apt_type(NumRoomsRem, TypeT, MinSizeT, _, _),
+	createAptRooms(FloorWidth, FloorHeight, AptTypeRem, RoomT, CoorT),
+	append(CoorH, CoorT, Coords).
+
+/****************************createRoom*********************************
+creates a single room
+**********************************************************************/
+createRoom(FloorWidth, FloorHeight, MinRoomSize, Room, Coord):-
+	create_rect_min_area(FloorWidth, FloorHeight, MinRoomSize, Room, Coord).
+
+/**************************createHallways*******************************
+creates a variable number of hallways using the helper createHallwaysHelper
+**********************************************************************/
+createHallways(FloorWidth, FloorHeight, TotalNumApts, NumHallways, Hallways, CoordList):-
+	length(Hallways, NumHallways),
+	NumHallways in 1..200,
+	createHallwaysHelper(FloorWidth, FloorHeight, TotalNumApts, Hallways, CoordList).
+	
+createHallwaysHelper(_, _, _, [], []).
+createHallwaysHelper(FloorWidth, FloorHeight, TotalNumApts, [HallH | HallT], Coord):-
+	create_rect(FloorWidth, FloorHeight, HallH, CoordH),
+	createHallwaysHelper(FloorWidth, FloorHeight, TotalNumApts, HallT, CoordT),
+	append(CoordH ,CoordT, Coord).
+	
+/***************************apts_util*********************************
+calculates the area of all appartments to check the utility
+uses the helper calc_util which calculates the area of a list of rectangles
+**********************************************************************/
+apts_util([], 0).
+apts_util([AH | AT], ApartmentsArea):-
+	check_util(AH, A1),
+	apts_util(AT, A2),
+	ApartmentsArea #= A1 + A2.
+	
+calc_util([], 0).	
+calc_util([H | T], Area):-
+	H = r(_, Width, _, Height),
+	A1 #= Width * Height,
+	calc_util(T, A2),
+	Area #= A1 + A2.
+	
+
+	
---
+++ rectangle_helpers.pl
@@ -0,0 +1,56 @@
+:-use_module(library(clpfd)).
+	
+create_rect(FloorWidth, FloorHeight, Rect, Coord):-
+	
+	Width in 1..FloorWidth,
+	Height in 1..FloorHeight,
+	
+	X #>= 0,
+	Y #>= 0,
+	X + Width #=< FloorWidth,
+	Y + Height #=< FloorHeight,
+	
+	Rect = r(X, Width, Y, Height),
+	Coord = [X, Y, Width, Height].
+	
+create_rect_min_area(FloorWidth, FloorHeight, MinRoomSize, Rect, Coord):-
+
+	Rect = r(_, Width, _, Height),
+	Width * Height #>= MinRoomSize,
+	create_rect(FloorWidth, FloorHeight, Rect, Coord).
+	
+adjacent(r(X1, W1, Y1, H1), r(X2, W2, Y2, H2)):-
+	
+	R1TopY #= Y1,
+	R1LefX #= X1,
+	R1BotY #= Y1 + H1,
+	R1RigX #= X1 + W1,
+	
+	R2TopY #= Y2,
+	R2LefX #= X2,
+	R2BotY #= Y2 + H2,
+	R2RigX #= X2 + W2,
+	
+	% r2 on top or at the bottom of r1
+	R1TopY #= R2BotY #<==> OnTop,
+	R1BotY #= R2TopY #<==> OnBot,
+	((OnTop #\/ OnBot) #/\ ((R2RigX #>= R1LefX #/\ R2RigX #=< R1RigX) #\/ (R2LefX #>= R1LefX #/\ R2LefX #=< R1RigX))) #<==> VertMidX, 
+	
+	TopBot #= OnTop + OnBot + VertMidX,
+	TopBot in 0\/2,
+
+	%r2 is to the left or right of 
+	R1LefX #= R2RigX #<==> OnLef,
+	R1RigX #= R2LefX #<==> OnRig,
+	((OnLef #\/ OnRig) #/\ ((R2TopY #>= R1TopY #/\ R2TopY #=< R1BotY) #\/ (R2BotY #>= R1TopY #/\ R2BotY #=< R1BotY))) #<==> VertMidY,
+	
+	LefRig #= OnLef + OnRig + VertMidY,
+	LefRig in 0\/2,
+	
+	Side #= OnTop + OnBot + OnLef + OnRig, 
+	Side #= 1.
+	
+% Test cases for adjacent
+%R1 = r(0,  5, 0,  5), R2 = r(5,  5, 0,  5), R3 = r(10, 5, 0,  5), R4 = r(0,  5, 5,  5), R5 = r(5,  5, 5,  5), R6 = r(10, 5, 5,  5), R7 = r(0,  5, 10, 5), R8 = r(5,  5, 10, 5), R9 = r(10, 5, 10, 5), adjacent(R8,R9).
+%R1 = r(0,  10, 0,  5), R2 = r(0,  6, 5,  17), R3 = r(6, 4, 5, 2), R4 = r(6, 4, 7, 15), adjacent(R1,R2).
+%R1 = r(0, 5, 0, 15), R2 = r(5, 5, 0, 5), R3 = r(5, 5, 5, 5), R4 = r(5, 5, 10, 5), adjacent(R1,R2).
